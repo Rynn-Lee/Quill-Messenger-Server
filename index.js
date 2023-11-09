da4e17066a59753b8d05d75540b0e12ab45fc861
Rynn Lee <20amigo04@gmail.com>
2023-11-09 16:34:16 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+//New imports
+const http = require('http').Server(app);
+const cors = require('cors');
+let clients = []
+
+app.use(cors());
+
+const socketIO = require('socket.io')(http, {
+  cors: {
+      origin: "http://localhost:3000"
+  }
+});
+
+//Add this before the app.get() block
+socketIO.on('connection', (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  clients.push(socket.id)
+
+  socket.on('getConnectedUsers', ()=>{
+    console.log("User", socket.id, "asked for connected clients")
+    socket.emit('connectedUsers', clients)
+  })
+
+  socket.on('disconnectAll', ()=>{
+    socketIO.sockets.sockets.map((user)=>{
+      user.di
+    })
+  })
+
+  socket.on('disconnect', () => {
+    clients = clients.filter((id) => id != socket.id)
+    console.log('🔥: A user disconnected');
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello world',
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
