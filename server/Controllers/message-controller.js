bcdcb8621d3daa7ac0774e141817303d5194b5cb
Rynn Lee <20amigo04@gmail.com>
2023-12-28 20:23:45 +0600

    Updated data returning after creating a message
    Removed $doc returning from server

--- server/Controllers/message-controller.js
+++ server/Controllers/message-controller.js
@@ -5,10 +5,9 @@ const createMessage = async(req, res) => {
   const message = new messageModel({
     chatID, senderID, text
   })
-
   try{
     const response = await message.save()
-    res.status(200).json(response)
+    res.status(200).json({_id: response.id, chatID, senderID, text, createdAt: response.createdAt, updatedAt: response.updatedAt})
   } catch (error) {
     console.log("An error occured on the server side!", error)
     res.status(500).json({message: error})
