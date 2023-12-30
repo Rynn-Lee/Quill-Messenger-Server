dc99b071dada4e93147a161d4928d3902a066a81
Rynn Lee <20amigo04@gmail.com>
2023-12-30 19:19:35 +0600

    Upgraded socket.io
    + Checks if _id is passed, otherwise disconnects the user
    + When user is connected to the server, collects all ids for this user for all connected devices

--- server/socket-io.js
+++ server/socket-io.js
@@ -1,28 +1,60 @@
-let clients = []
+const { findUser } = require('./Controllers/user-controller');
+
 const port = process.env.CLIENT_LISTEN_PORT
 console.log("port:", port)
+
+let connectedUsers = {}
 
 const socketConnection = (http) => {
   const socketIO = require('socket.io')(http, {
     cors: {
-        origin: `http://localhost:${port}`
+        origin: [`http://localhost:3000`, `http://localhost:3001`, `http://localhost:19000`, `http://localhost:13131`]
     }
   });
   
   socketIO.on('connection', (socket) => {
     console.log(`⚡: ${socket.id} user just connected!`);
-    clients.push(socket.id)
-  
+    const user = socket.handshake.query._id
+    if(!user){
+      socket.disconnect()
+      return
+    }
+
+    socket.on('connectDevice', (data) => {
+      console.log("Connected user data", data)
+      addId({usertag: data.usertag, _id: socket.id})
+    })
+
     socket.on('getConnectedUsers', ()=>{
       console.log("User", socket.id, "asked for connected clients")
-      socket.emit('connectedUsers', clients)
+      socket.emit('connectedUsers', connectedUsers)
     })
   
     socket.on('disconnect', (reason) => {
-      clients = clients.filter((id) => id != socket.id)
+      removeId(socket.id)
       console.log('🔥: A user disconnected: ', reason);
     });
   });
+}
+
+const addId = (data) => {
+  if (!connectedUsers[data.usertag]) {
+    connectedUsers[data.usertag] = [];
+  }
+  connectedUsers[data.usertag].push(data._id);
+  console.log("connectedUsers", connectedUsers)
+}
+
+// Function to remove an id from a user
+const removeId = (id) => {
+ for (let user in connectedUsers) {
+    let index = connectedUsers[user].indexOf(id);
+    if (index !== -1) {
+      connectedUsers[user].splice(index, 1);
+      break;
+    }
+  }
+  console.log("connectedUsers", connectedUsers)
 }
 
 module.exports = (http) => socketConnection(http)
