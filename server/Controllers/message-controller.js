948fc144f06d4eee4b50c3145ccec8174d44e361
Everrynn <20amigo04@gmail.com>
2024-06-03 10:38:48 +0500

    OnlineAtlas

--- server/Controllers/message-controller.js
+++ server/Controllers/message-controller.js
@@ -26,20 +26,6 @@ const createMessage = async(req, res) => {
   }
 }
 
-// const createMessage = async(req, res) => {
-//   const {chatID, senderID, text} = req.body
-//   const message = new messageModel({
-//     chatID, senderID, text
-//   })
-//   try{
-//     const response = await message.save()
-//     res.status(200).json({_id: response.id, chatID, senderID, text, createdAt: response.createdAt, updatedAt: response.updatedAt})
-//   } catch (error) {
-//     console.log("An error occured on the server side!", error)
-//     res.status(500).json({message: error})
-//   }
-// }
-
 const getMessages = async(req, res) => {
   const {chatID} = req.params
   try{
--- server/utils/DBconnect.js
+++ server/utils/DBconnect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://localhost:27017/quill-messenger'
+// const url = 'mongodb://localhost:27017/quill-messenger'
+const url = 'mongodb+srv://DiplomaDB:123123123123@diplomacluster.cshxctx.mongodb.net/?retryWrites=true&w=majority&appName=DiplomaCluster'
 
 mongoose.connect(url).then(()=>{
   console.log("MONGO DB: Connected to database!")
